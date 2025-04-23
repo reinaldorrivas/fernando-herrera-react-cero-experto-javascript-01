7902ca0d888191e160c59cbb13ef87c356944049
Rene Rivas <rrivas@teravisiontech.com>
2025-04-23 12:42:16 -0400

    fh-react-js-basics: desestructuración

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const superHeroe = {
+  name: 'Superman',
+  age: 30,
+  powers: ['flight', 'super strength', 'x-ray vision'],
+  isAlive: true,
+  friends: {
+    name: 'Batman',
+    age: 35,
+    powers: ['martial arts', 'intelligence'],
+    isAlive: true,
+  },
+};
+
+console.table(superHeroe);
