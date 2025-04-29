6a32c51052207345adc56a76ee279e62f9e32a6f
Rene Rivas <rrivas@teravisiontech.com>
2025-04-29 10:59:20 -0400

    fh-reatct-js-basics: array destructuring

--- src/index.js
+++ src/index.js
@@ -1,51 +1,95 @@
-const superhero = {
-  realName: 'Clark Kent',
-  nickname: 'Superman',
-  age: 30,
-  powers: ['flight', 'super strength', 'x-ray vision'],
-  isAlive: true,
-  friends: {
-    nickname: 'Batman',
-    realName: 'Bruce Wayne',
-    age: 35,
-    powers: ['martial arts', 'intelligence'],
-    isAlive: true,
-  },
+const personajesDragonBall = [
+  'Goku',
+  'Vegeta',
+  'Gohan',
+  'Piccolo',
+  'Bulma',
+  'Krillin',
+  'Frieza',
+  'Cell',
+  'Majin Buu',
+  'Trunks',
+];
+
+// Desestructuración de arreglos:
+// La desestructuración de arreglos se hace de la misma manera que la de objetos, pero con corchetes.
+// La diferencia radica en que necesitas usar el mismo orden en el que están los elementos del arreglo.
+// En este caso, el primer elemento del arreglo se asigna a la primera variable, el segundo elemento a la segunda variable, y así sucesivamente.
+// En caso de requerir un elemento que está después de otro, puedes usar comas para omitir los elementos que no necesitas.
+const [goku, , gohan] = personajesDragonBall;
+
+console.log(`Goku: ${goku}, Gohan: ${gohan}`);
+
+//Si una función retorna un arreglo, puedes desestructurarlo de la misma manera que lo harías con un objeto.
+const getDragonBallCharacters = () => {
+  return [
+    'Goku',
+    'Vegeta',
+    'Gohan',
+    'Piccolo',
+    'Bulma',
+    'Krillin',
+    'Frieza',
+    'Cell',
+    'Majin Buu',
+    'Trunks',
+  ];
 };
 
-console.table(superhero);
+const [, , , piccolo, , , , cell] = getDragonBallCharacters();
 
-// Desestructuración de un objeto
-const { realName: realName, nickname, age } = superhero;
+console.log(`Piccolo: ${piccolo}, Cell: ${cell}`);
 
-console.log('Name: ', realName, 'Nickname: ', nickname, 'Age: ', age);
+// Desestructuración de arreglos anidados:
+const personajesDragonBallAnidado = [
+  ['Goku', 'Vegeta', 'Gohan'],
+  ['Piccolo', 'Bulma', 'Krillin'],
+  ['Frieza', 'Cell', 'Majin Buu'],
+  ['Trunks'],
+];
+const [, [, bulma, krillin], [, , majinBuu]] = personajesDragonBallAnidado;
+console.log(`Bulma: ${bulma}, Krillin: ${krillin}, Majin Buu: ${majinBuu}`);
 
-// Desestructuración de un objeto anidado
-const {
-  friend: {
-    realName: friendRealName,
-    nickname: friendNickname,
-    age: friendAge,
-  },
-} = superhero;
+// Desestructuración de arreglos con objetos:
+// Si una función retorna un arreglo de objetos, puedes desestructurarlo de la misma manera que lo harías con un objeto.
+// En este caso, puedes usar la misma sintaxis de desestructuración que usaste antes, pero ahora necesitas usar llaves para acceder a las propiedades del objeto.
+const getDragonBallCharactersWithDescription = () => {
+  return [
+    {
+      name: 'Goku',
+      age: 30,
+      power: 'Kamehameha',
+    },
+    {
+      name: 'Vegeta',
+      age: 35,
+      power: 'Big Bang Attack',
+    },
+    {
+      name: 'Gohan',
+      age: 25,
+      power: 'Masenko',
+    },
+  ];
+};
+const [, { name: vegeta, age: vegetaAge, power: vegetaPower }] =
+  getDragonBallCharactersWithDescription();
+
+console.log(`Vegeta: ${vegeta}, Age: ${vegetaAge}, Power: ${vegetaPower}`);
+
+// Se puede desestructurar un arreglo de objetos y asignar el objeto a una variable:
+const [, vegetaObj] = getDragonBallCharactersWithDescription();
+
+console.log(vegetaObj);
+
+// Desestructuración de un arreglo retornado por una función con paso de parámetros:
+const getDragonBallCharacterArray = (character) => {
+  return [character, 30, 'Kamehameha'];
+};
+
+const [characterName, characterAge, characterPower] =
+  getDragonBallCharacterArray('Goku');
 
 console.log(
-  'Friend real name: ',
-  friendRealName,
-  'Friend nickname: ',
-  friendNickname,
-  'Friend age: ',
-  friendAge
+  `Character: ${characterName}, Age: ${characterAge}, Power: ${characterPower}`
 );
-
-const superheroInfo = ({ nickname, age, range = 'Captain' }) => {
-  console.log('Hello, ' + range + '!');
-
-  return {
-    superheroNickname: nickname,
-    superheroAge: age,
-  };
-};
-
-const { superheroNickname, superheroAge } = superheroInfo(superhero);
-console.log('superhero: ', superheroNickname, 'superhero age: ', superheroAge);
